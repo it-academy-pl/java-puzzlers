daa464670aba9637f89741b3fb144d9034cdf6cc
Rafal Bednarz <raf.bednarz@gmail.com>
2019-07-06 09:40:59 +0200

    builder pattern

--- src/pl/itacademy/design_patterns/builder/Main.java
+++ src/pl/itacademy/design_patterns/builder/Main.java
@@ -2,6 +2,7 @@ package pl.itacademy.design_patterns.builder;
 
 public class Main {
     public static void main(String[] args) {
-
+        Person person = Person.PersonBuilder.builder().address("Krakow").lastName("Bednarz").firstName("Rafal").build();
+        System.out.println(person);
     }
 }
--- src/pl/itacademy/design_patterns/builder/Person.java
+++ src/pl/itacademy/design_patterns/builder/Person.java
@@ -8,8 +8,73 @@ public class Person {
     private String pesel;
     private String nip;
     private String address;
-
     public enum Gender {MALE, FEMALE}
+
+    private Person () {}
+
+    public static final class PersonBuilder {
+        private String firstName;
+        private Gender gender;
+        private String lastName;
+        private String phoneNumber;
+        private String pesel;
+        private String nip;
+        private String address;
+
+        private PersonBuilder() {
+        }
+
+        public static PersonBuilder aPerson() {
+            return new PersonBuilder();
+        }
+
+        public PersonBuilder withFirstName(String firstName) {
+            this.firstName = firstName;
+            return this;
+        }
+
+        public PersonBuilder withGender(Gender gender) {
+            this.gender = gender;
+            return this;
+        }
+
+        public PersonBuilder withLastName(String lastName) {
+            this.lastName = lastName;
+            return this;
+        }
+
+        public PersonBuilder withPhoneNumber(String phoneNumber) {
+            this.phoneNumber = phoneNumber;
+            return this;
+        }
+
+        public PersonBuilder withPesel(String pesel) {
+            this.pesel = pesel;
+            return this;
+        }
+
+        public PersonBuilder withNip(String nip) {
+            this.nip = nip;
+            return this;
+        }
+
+        public PersonBuilder withAddress(String address) {
+            this.address = address;
+            return this;
+        }
+
+        public Person build() {
+            Person person = new Person();
+            person.gender = this.gender;
+            person.phoneNumber = this.phoneNumber;
+            person.nip = this.nip;
+            person.pesel = this.pesel;
+            person.lastName = this.lastName;
+            person.address = this.address;
+            person.firstName = this.firstName;
+            return person;
+        }
+    }
 
     @Override
     public String toString() {
